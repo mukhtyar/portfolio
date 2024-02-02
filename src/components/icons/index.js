31804ce23df802a73537f44d2e38a804ed815959
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2024-02-02 12:00:17 -0800

    add kaggle icon

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -2,3 +2,4 @@ export { default as IconGithub } from "./Github.svelte";
 export { default as IconTwitter } from "./Twitter.svelte";
 export { default as IconObservable } from "./Observable.svelte";
 export { default as IconLinkedIn } from "./LinkedIn.svelte";
+export { default as IconKaggle } from "./Kaggle.svelte";
